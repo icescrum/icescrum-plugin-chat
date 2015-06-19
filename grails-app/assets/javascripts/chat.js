7b8dae7c37eaf4b4afc3923cd487bb6c4f9693db
Nicolas Noullet <nnoullet@kagilum.com>
2015-06-19 11:45:48 +0200

    New resource config

---
+++ grails-app/assets/javascripts/chat.js
@@ -0,0 +1,9 @@
+//= encoding     UTF-8
+//= require      chat/vendors/flXHR/flXHR
+//= require_tree chat/vendors/flXHR
+//= require      chat/vendors/strophe/strophe.min
+//= require_tree chat/vendors/strophe/
+//= require_tree chat/vendors/jquery
+//= require_tree chat
+
+// Note: this is NOT bundled in iceScrum, this should be included only if chat is enabled
